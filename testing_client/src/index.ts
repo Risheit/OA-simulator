811f8e5536a55c22d4aa0e5a6cf1d84dffcf426d
Risheit Munshi <Risheit.Munshi@mail.utoronto.ca>
2026-09-20 23:23:59 -0500

    Add file editing functionality

--- testing_client/src/index.ts
+++ testing_client/src/index.ts
@@ -4,40 +4,120 @@ import express, {
   type Request,
   type Response,
 } from "express";
-import { env, exit } from "node:process";
+import { env } from "node:process";
 import { existsSync } from "node:fs";
-import { resetSandbox } from "./sandbox.js";
+import {
+  lsSandbox,
+  readSandboxFile,
+  resetSandbox,
+  rmSandboxFile,
+  writeSandboxFile,
+} from "./sandbox.js";
+import {
+  assertEnvVariableDefined,
+  InvalidRequest,
+  InvalidFilePathError,
+  isInvalidFilePath,
+  InvalidRequestError,
+} from "./errors.js";
+import { contentType } from "mime-types";
 
-if (!env.PORT) {
-  console.error("No PORT env variable found!");
-  exit(1);
-}
-
-if (!env.SANDBOX_PATH) {
-  console.error("No SANDBOX_PATH env variable found!");
-  exit(1);
-}
+assertEnvVariableDefined("PORT", env.PORT); // Server port
+assertEnvVariableDefined("SANDBOX_PATH", env.SANDBOX_PATH); // Absolute file path to sandbox
 
 const app: Express = express();
 
 const config = {
   port: env.PORT!,
-  path: env.SANDBOX_PATH!,
+  sandboxPath: env.SANDBOX_PATH!,
 };
 
-app.get("/health", async (_: Request, res: Response) => {
-  if (existsSync(config.path)) {
+app.get("/health", async (_, res) => {
+  if (existsSync(config.sandboxPath)) {
     res.sendStatus(200);
   } else {
     res.sendStatus(503);
   }
 });
 
-app.post("/clean", (_: Request, res: Response) => {
-  resetSandbox(config.path);
+app.post("/clean", (_, res) => {
+  resetSandbox(config.sandboxPath);
   res.sendStatus(200);
 });
 
+app.get("/sandbox/ls/*filepath", async (req, res) => {
+  const path = req.params.filepath.join("/");
+
+  if (isInvalidFilePath(path)) {
+    throw new InvalidFilePathError();
+  }
+
+  const result = await lsSandbox(config.sandboxPath, path);
+  if (!result.ok) {
+    throw new InvalidRequestError(result.err);
+  }
+
+  res.status(200).json(result.val);
+});
+
+app.use("/sandbox/file/*filepath", express.raw({ type: "*/*", limit: "10mb" }));
+
+app.get("/sandbox/file/*filepath", async (req, res) => {
+  const path = req.params.filepath.join("/");
+
+  if (isInvalidFilePath(path)) {
+    throw new InvalidFilePathError();
+  }
+
+  const result = await readSandboxFile(config.sandboxPath, path);
+  if (!result.ok) {
+    throw new InvalidRequestError(result.err);
+  }
+
+  const httpContentType = contentType(path) || "application/octet-stream";
+  res.status(200).contentType(httpContentType).send(result.val);
+});
+
+app.post("/sandbox/file/*filepath", async (req, res) => {
+  const path = req.params.filepath.join("/");
+
+  if (isInvalidFilePath(path)) {
+    throw new InvalidFilePathError();
+  }
+
+  const result = await writeSandboxFile(config.sandboxPath, path, req.body);
+  if (!result.ok) {
+    throw new InvalidRequestError(result.err);
+  }
+
+  res.sendStatus(201);
+});
+
+app.delete("/sandbox/file/*filepath", async (req, res) => {
+  const path = req.params.filepath.join("/");
+
+  if (isInvalidFilePath(path)) {
+    throw new InvalidFilePathError();
+  }
+
+  const result = await rmSandboxFile(config.sandboxPath, path);
+  if (!result.ok) {
+    throw new InvalidRequestError(result.err);
+  }
+
+  res.sendStatus(200);
+});
+
+// Invalid request handler
+app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
+  if (err.cause !== InvalidRequest) {
+    next(err);
+  }
+
+  res.status(400).json({ error: err.message });
+});
+
+// Default error handler
 app.use((err: Error, _: Request, res: Response, __: NextFunction) => {
   res.status(500).json({ error: err.message });
 });
--- testing_client/src/sandbox.ts
+++ testing_client/src/sandbox.ts
@@ -1,7 +1,123 @@
-import { rm, mkdir } from "node:fs/promises";
+import {
+  rm,
+  mkdir,
+  writeFile,
+  readFile,
+  stat,
+  readdir,
+} from "node:fs/promises";
+import { dirname, isAbsolute, join, relative, resolve } from "node:path";
+import { ok, Result } from "./errors.js";
+import { fail } from "node:assert";
+import { existsSync } from "node:fs";
+
+const failFileNotFound = () => fail("File not found");
+const failInvalidAccess = () => fail("Accessing files outside of sandbox");
+
+function isOutsideSandbox(sandboxPath: string, path: string) {
+  const relativePath = relative(sandboxPath, path);
+
+  return relativePath.startsWith("..") || isAbsolute(relativePath);
+}
 
 export async function resetSandbox(sandboxPath: string) {
   await rm(sandboxPath, { recursive: true, force: true });
   await mkdir(sandboxPath, { recursive: true });
 }
 
+type ReadFileResult = Awaited<ReturnType<typeof readFile>>;
+export async function readSandboxFile(
+  sandboxPath: string,
+  filePath: string,
+): Promise<Result<ReadFileResult>> {
+  const fullPath = join(sandboxPath, filePath);
+
+  if (isOutsideSandbox(sandboxPath, fullPath)) {
+    return failInvalidAccess();
+  }
+
+  if (!existsSync(fullPath)) {
+    return failFileNotFound();
+  }
+
+  const fileMetadata = await stat(fullPath);
+  if (fileMetadata.isDirectory()) {
+    return fail("Trying to read a directory.");
+  }
+
+  const buffer = await readFile(fullPath);
+  return ok(buffer);
+}
+
+type ValidFileType = Parameters<typeof writeFile>[1];
+export async function writeSandboxFile(
+  sandboxPath: string,
+  filePath: string,
+  file: ValidFileType,
+): Promise<Result<void>> {
+  const fullPath = join(sandboxPath, filePath);
+
+  if (isOutsideSandbox(sandboxPath, fullPath)) {
+    return failInvalidAccess();
+  }
+
+  if (resolve(sandboxPath) === resolve(fullPath)) {
+    return fail("Cannot delete root");
+  }
+
+  await mkdir(dirname(fullPath), { recursive: true });
+  await writeFile(fullPath, file);
+
+  return ok();
+}
+
+export async function rmSandboxFile(
+  sandboxPath: string,
+  filePath: string,
+): Promise<Result<void>> {
+  const fullPath = join(sandboxPath, filePath);
+
+  if (isOutsideSandbox(sandboxPath, fullPath)) {
+    return failInvalidAccess();
+  }
+
+  if (!existsSync(fullPath)) {
+    return failFileNotFound();
+  }
+
+  await rm(fullPath, { recursive: true });
+  return ok();
+}
+
+interface DirectoryEntry {
+  name: string;
+  isDirectory: boolean;
+}
+
+export async function lsSandbox(
+  sandboxPath: string,
+  filePath: string,
+): Promise<Result<DirectoryEntry[]>> {
+  const fullPath = join(sandboxPath, filePath);
+
+  if (isOutsideSandbox(sandboxPath, fullPath)) {
+    return failInvalidAccess();
+  }
+
+  if (!existsSync(fullPath)) {
+    return failFileNotFound();
+  }
+
+  const fileMetadata = await stat(fullPath);
+  if (!fileMetadata.isDirectory()) {
+    return fail("Trying to read something that isn't directory.");
+  }
+
+  const entries = await readdir(fullPath, { withFileTypes: true });
+  const cleanedEntries = entries.map((entry) => ({
+    name: entry.name,
+    isDirectory: entry.isDirectory(),
+  }));
+
+  return ok(cleanedEntries);
+}
